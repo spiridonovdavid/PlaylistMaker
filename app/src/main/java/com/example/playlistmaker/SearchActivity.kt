e15d42e134b40444030e695619df6461cd16b903
David Spiridonov <spiridonovdavid@gmail.com>
2024-07-06 04:39:58 +0300

    Спринт 9 дз

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,78 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var inputView: EditText? = null
+    private var inputValue: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<View>(R.id.back)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+        inputView = findViewById(R.id.inputView)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // пусто
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                inputValue = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // пусто
+            }
+        }
+
+        inputView?.addTextChangedListener(simpleTextWatcher)
+
+        clearButton.setOnClickListener {
+            inputView?.setText("")
+            val hideKeyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideKeyboard.hideSoftInputFromWindow(inputView?.windowToken, 0)
+            inputView?.clearFocus()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STRING_VALUE, inputValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputValue = savedInstanceState.getString(STRING_VALUE, STRING_DEFAULT)
+        inputView?.setText(inputValue)
+    }
+
+    companion object {
+        const val STRING_VALUE = "PRODUCT_AMOUNT"
+        const val STRING_DEFAULT = ""
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
