a8ce35b1f0e62e2ccb40249287bf8c2d63474839
David Spiridonov <spiridonovdavid@gmail.com>
2024-07-11 02:05:12 +0300

    10 домашнее задание фиксы по рекомендациям

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,8 @@
 package com.practicum.playlistmaker
 
 data class Track(
-    var trackName: String,
-    var artistName: String,
-    var trackTime: String,
-    var trackPhoto: String
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val trackPhoto: String
 )
